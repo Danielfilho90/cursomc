b808eeb0d8a0961d2246c489934ded1b129ad4e7
Daniel Filho <danielfilho90@gmail.com>
2022-02-01 09:58:50 -0300

    Criando o repository e o service para categoria

---
+++ src/main/java/com/daniel/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.daniel.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.daniel.domain.Categoria;
+import com.daniel.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}	
+}
